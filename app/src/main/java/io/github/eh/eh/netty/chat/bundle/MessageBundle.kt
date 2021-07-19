dc6efaba7e6c17ab5149c4b6471fa368e64f3789
Singlerr <singlerr@soongsil.ac.kr>
2021-07-19 15:42:19 +0900

    유서웅: Fixed 'kotlin plugin' error

--- app/src/main/java/io/github/eh/eh/netty/chat/bundle/MessageBundle.kt
+++ app/src/main/java/io/github/eh/eh/netty/chat/bundle/MessageBundle.kt
@@ -1,9 +1,6 @@
 package io.github.eh.eh.netty.chat.bundle
 
-import lombok.Getter
-
-@Getter
-class MessageBundle private constructor(private val message: String) {
+class MessageBundle private constructor(val message: String) {
     companion object {
         fun createMessage(text: String): MessageBundle {
             return MessageBundle(text)
